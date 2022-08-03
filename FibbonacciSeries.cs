30ae94e885f6f22d29b40ad71a008d75c5f6112f
Anisha0906 <anishadas880@gmail.com>
2022-08-03 20:16:30 +0530

    Add: Fibonacci Series

---
+++ FibbonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrgrams
+{
+    class FibbonacciSeries
+    {
+        public void CalculateFibonacciSeries()
+        {
+            int number, n1 = 0, n2 = 1, n3, i;
+            Console.WriteLine("Enter the number of elements");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1+ " " + n2 + "  ");
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrgrams
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problems ");
+            Console.WriteLine("Enter your choice ");
+            Console.WriteLine(".............................................");
+            Console.WriteLine("1 For: Fibonacci Series ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    FibbonacciSeries FibboSeries = new FibbonacciSeries();
+                    FibboSeries.CalculateFibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection.select 1");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
